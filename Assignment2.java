bdf33d6a9d3733ddd027da225afd184aabd9dd8a
= <=>
2024-05-09 22:14:53 +1200

    added resource files, added starting code

--- Assignment2.java
+++ Assignment2.java
@@ -1,7 +1,40 @@
-class Assignment2
+/*
+    Assignment 2
+
+    Created by:
+        Lyle Hunt
+        Jamie Nicholson
+        Tony Benefield
+        Nathan Burr
+ */
+
+public class Assignment2 extends GameEngine
 {
+    static int framerate = 30;
+    
     public static void main(String[] args) 
     {
-        System.out.println("Hello World");    
+        createGame(new Assignment2(), framerate);
     }
+    
+    public void init() 
+    {
+        setWindowSize(600, 600);
+    }
+
+    public void update(double dt) {}
+
+    public void paintComponent() {}
+
+    /*
+     * TODO:
+     * 
+     * Maze generation algorithm
+     * (Sprite) Animations
+     * AI and Player Movement
+     * Collisions
+     * Interactable items (keys, doors)
+     * Menu/Player Options
+     *  
+     */
 }
